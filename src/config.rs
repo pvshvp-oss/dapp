27f6e769d570654ad407b2c074ef949ecfb42cca
shiva-patt-oss <shiva.patt.oss@gmail.com>
2023-09-10 02:29:50 -0500

    chore: :truck: Change username and email

--- src/config.rs
+++ src/config.rs
@@ -48,15 +48,11 @@ pub trait Configuration: Default {
     /// anywhere in a similar manner.
     fn string<'de, D>(&mut self, config_string: &'de str) -> Result<&mut Self, Error>
     where
-        Self: Deserialize<'de>,
-        D: Deserializer<'de> + From<&'de str>,
+        Self: Deserialize<'de> + 'de,
+        D: ConfigDeserialize<'de, Self, Error = Box<dyn std::error::Error + 'static>>,
     {
-        let other_config = Self::deserialize(D::from(config_string))
-            .map_err(|serde_error| {
-                eprintln!("{:#?}", serde_error);
-                // Box::from(serde_error)
-                Box::from("Hello")
-            })
+        let other_config = D::try_config_from_string(config_string)
+            .map_err(|serde_error| Box::from(serde_error))
             .context(ParseConfigStringSnafu {
                 string: config_string.clone(),
             })?;
@@ -74,8 +70,8 @@ pub trait Configuration: Default {
     /// method must call `self.set_loaded()` if any fields were set/modified.  
     fn filepath<'de, D>(&mut self, config_filepath: impl AsRef<Path>) -> Result<&mut Self, Error>
     where
-        Self: Deserialize<'de>,
-        D: Deserializer<'de> + From<BufReader<File>>,
+        Self: Deserialize<'de> + 'de,
+        D: ConfigDeserialize<'de, Self, Error = Box<dyn std::error::Error + 'static>>,
     {
         let config_filepath = config_filepath.as_ref().to_owned();
         if !config_filepath.exists() {
@@ -85,12 +81,8 @@ pub trait Configuration: Default {
                 path: config_filepath.clone(),
             })?;
             let file_reader = BufReader::new(file);
-            let other_config = Self::deserialize(D::from(file_reader))
-                .map_err(|serde_error| {
-                    eprintln!("{:#?}", serde_error);
-                    // Box::from(serde_error)
-                    Box::from("Hello")
-                })
+            let other_config = D::try_config_from_reader(file_reader)
+                .map_err(|serde_error| Box::from(serde_error))
                 .context(ParseConfigFileSnafu {
                     path: config_filepath.clone(),
                 })?;
@@ -107,8 +99,8 @@ pub trait Configuration: Default {
         optional_config_filepath: Option<impl AsRef<Path>>,
     ) -> Result<&mut Self, Error>
     where
-        Self: Deserialize<'de>,
-        D: Deserializer<'de> + From<BufReader<File>> + 'de,
+        Self: Deserialize<'de> + 'de,
+        D: ConfigDeserialize<'de, Self, Error = Box<dyn std::error::Error + 'static>>,
     {
         match optional_config_filepath {
             Some(config_filepath) => self.filepath::<D>(config_filepath),
@@ -124,8 +116,8 @@ pub trait Configuration: Default {
         config_filepath: impl AsRef<Path>,
     ) -> Result<&mut Self, Error>
     where
-        Self: Deserialize<'de>,
-        D: Deserializer<'de> + From<BufReader<File>>,
+        Self: Deserialize<'de> + 'de,
+        D: ConfigDeserialize<'de, Self, Error = Box<dyn std::error::Error + 'static>>,
     {
         if !config_filepath.exists() {
             Err(Error::FindConfigFile {
@@ -143,8 +135,8 @@ pub trait Configuration: Default {
         optional_config_filepath: Option<impl AsRef<Path>>,
     ) -> Result<&mut Self, Error>
     where
-        Self: Deserialize<'de>,
-        D: Deserializer<'de> + From<BufReader<File>>,
+        Self: Deserialize<'de> + 'de,
+        D: ConfigDeserialize<'de, Self, Error = Box<dyn std::error::Error + 'static>>,
     {
         match optional_config_filepath {
             Some(config_filepath) => self.try_filepath::<D>(config_filepath),
@@ -178,385 +170,55 @@ pub trait Configuration: Default {
     }
 }
 
-// region: REMOTE TEMPLATES
-
-#[doc(hidden)]
 #[cfg(feature = "serde")]
-#[delegatable_trait_remote]
-/// A copy of the remote trait signature to delegate to a newtype
-trait Deserializer<'de>: Sized {
-    /// The error type that can be returned if some error occurs during
-    /// deserialization.
-    type Error: Error;
+pub trait ConfigDeserialize<'de, C>
+where
+    C: Configuration + 'de,
+{
+    type Error;
 
-    /// Require the `Deserializer` to figure out how to drive the visitor based
-    /// on what data type is in the input.
-    ///
-    /// When implementing `Deserialize`, you should avoid relying on
-    /// `Deserializer::deserialize_any` unless you need to be told by the
-    /// Deserializer what type is in the input. Know that relying on
-    /// `Deserializer::deserialize_any` means your data type will be able to
-    /// deserialize from self-describing formats only, ruling out Postcard and
-    /// many others.
-    fn deserialize_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
+    fn try_config_from_reader(reader: impl std::io::Read) -> Result<C, Self::Error>;
 
-    /// Hint that the `Deserialize` type is expecting a `bool` value.
-    fn deserialize_bool<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an `i8` value.
-    fn deserialize_i8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an `i16` value.
-    fn deserialize_i16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an `i32` value.
-    fn deserialize_i32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an `i64` value.
-    fn deserialize_i64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an `i128` value.
-    ///
-    /// The default behavior unconditionally returns an error.
-    fn deserialize_i128<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>,
-    {
-        let _ = visitor;
-        Err(Error::custom("i128 is not supported"))
-    }
-
-    /// Hint that the `Deserialize` type is expecting a `u8` value.
-    fn deserialize_u8<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a `u16` value.
-    fn deserialize_u16<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a `u32` value.
-    fn deserialize_u32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a `u64` value.
-    fn deserialize_u64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an `u128` value.
-    ///
-    /// The default behavior unconditionally returns an error.
-    fn deserialize_u128<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>,
-    {
-        let _ = visitor;
-        Err(Error::custom("u128 is not supported"))
-    }
-
-    /// Hint that the `Deserialize` type is expecting a `f32` value.
-    fn deserialize_f32<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a `f64` value.
-    fn deserialize_f64<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a `char` value.
-    fn deserialize_char<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a string value and does
-    /// not benefit from taking ownership of buffered data owned by the
-    /// `Deserializer`.
-    ///
-    /// If the `Visitor` would benefit from taking ownership of `String` data,
-    /// indicate this to the `Deserializer` by using `deserialize_string`
-    /// instead.
-    fn deserialize_str<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a string value and would
-    /// benefit from taking ownership of buffered data owned by the
-    /// `Deserializer`.
-    ///
-    /// If the `Visitor` would not benefit from taking ownership of `String`
-    /// data, indicate that to the `Deserializer` by using `deserialize_str`
-    /// instead.
-    fn deserialize_string<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a byte array and does not
-    /// benefit from taking ownership of buffered data owned by the
-    /// `Deserializer`.
-    ///
-    /// If the `Visitor` would benefit from taking ownership of `Vec<u8>` data,
-    /// indicate this to the `Deserializer` by using `deserialize_byte_buf`
-    /// instead.
-    fn deserialize_bytes<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a byte array and would
-    /// benefit from taking ownership of buffered data owned by the
-    /// `Deserializer`.
-    ///
-    /// If the `Visitor` would not benefit from taking ownership of `Vec<u8>`
-    /// data, indicate that to the `Deserializer` by using `deserialize_bytes`
-    /// instead.
-    fn deserialize_byte_buf<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an optional value.
-    ///
-    /// This allows deserializers that encode an optional value as a nullable
-    /// value to convert the null value into `None` and a regular value into
-    /// `Some(value)`.
-    fn deserialize_option<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a unit value.
-    fn deserialize_unit<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a unit struct with a
-    /// particular name.
-    fn deserialize_unit_struct<V>(
-        self,
-        name: &'static str,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a newtype struct with a
-    /// particular name.
-    fn deserialize_newtype_struct<V>(
-        self,
-        name: &'static str,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a sequence of values.
-    fn deserialize_seq<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a sequence of values and
-    /// knows how many values there are without looking at the serialized data.
-    fn deserialize_tuple<V>(self, len: usize, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a tuple struct with a
-    /// particular name and number of fields.
-    fn deserialize_tuple_struct<V>(
-        self,
-        name: &'static str,
-        len: usize,
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a map of key-value pairs.
-    fn deserialize_map<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting a struct with a particular
-    /// name and fields.
-    fn deserialize_struct<V>(
-        self,
-        name: &'static str,
-        fields: &'static [&'static str],
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting an enum value with a
-    /// particular name and possible variants.
-    fn deserialize_enum<V>(
-        self,
-        name: &'static str,
-        variants: &'static [&'static str],
-        visitor: V,
-    ) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type is expecting the name of a struct
-    /// field or the discriminant of an enum variant.
-    fn deserialize_identifier<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Hint that the `Deserialize` type needs to deserialize a value whose type
-    /// doesn't matter because it is ignored.
-    ///
-    /// Deserializers for non-self-describing formats may not support this mode.
-    fn deserialize_ignored_any<V>(self, visitor: V) -> Result<V::Value, Self::Error>
-    where
-        V: Visitor<'de>;
-
-    /// Determine whether `Deserialize` implementations should expect to
-    /// deserialize their human-readable form.
-    ///
-    /// Some types have a human-readable form that may be somewhat expensive to
-    /// construct, as well as a binary form that is compact and efficient.
-    /// Generally text-based formats like JSON and YAML will prefer to use the
-    /// human-readable one and binary formats like Postcard will prefer the
-    /// compact one.
-    ///
-    /// ```edition2021
-    /// # use std::ops::Add;
-    /// # use std::str::FromStr;
-    /// #
-    /// # struct Timestamp;
-    /// #
-    /// # impl Timestamp {
-    /// #     const EPOCH: Timestamp = Timestamp;
-    /// # }
-    /// #
-    /// # impl FromStr for Timestamp {
-    /// #     type Err = String;
-    /// #     fn from_str(_: &str) -> Result<Self, Self::Err> {
-    /// #         unimplemented!()
-    /// #     }
-    /// # }
-    /// #
-    /// # struct Duration;
-    /// #
-    /// # impl Duration {
-    /// #     fn seconds(_: u64) -> Self { unimplemented!() }
-    /// # }
-    /// #
-    /// # impl Add<Duration> for Timestamp {
-    /// #     type Output = Timestamp;
-    /// #     fn add(self, _: Duration) -> Self::Output {
-    /// #         unimplemented!()
-    /// #     }
-    /// # }
-    /// #
-    /// use serde::de::{self, Deserialize, Deserializer};
-    ///
-    /// impl<'de> Deserialize<'de> for Timestamp {
-    ///     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    ///     where
-    ///         D: Deserializer<'de>,
-    ///     {
-    ///         if deserializer.is_human_readable() {
-    ///             // Deserialize from a human-readable string like "2015-05-15T17:01:00Z".
-    ///             let s = String::deserialize(deserializer)?;
-    ///             Timestamp::from_str(&s).map_err(de::Error::custom)
-    ///         } else {
-    ///             // Deserialize from a compact binary representation, seconds since
-    ///             // the Unix epoch.
-    ///             let n = u64::deserialize(deserializer)?;
-    ///             Ok(Timestamp::EPOCH + Duration::seconds(n))
-    ///         }
-    ///     }
-    /// }
-    /// ```
-    ///
-    /// The default implementation of this method returns `true`. Data formats
-    /// may override this to `false` to request a compact form for types that
-    /// support one. Note that modifying this method to change a format from
-    /// human-readable to compact or vice versa should be regarded as a breaking
-    /// change, as a value serialized in human-readable mode is not required to
-    /// deserialize from the same data in compact mode.
-    #[inline]
-    fn is_human_readable(&self) -> bool {
-        true
-    }
+    fn try_config_from_string(string: &'de str) -> Result<C, Self::Error>;
 }
-
-// endregion: REMOTE TEMPLATES
 
 // region: FORMAT IMPLEMENTATIONS
 
 #[cfg(feature = "yaml")]
-#[derive(Delegate)]
-#[delegate(Deserializer<'de1>, generics = "'de1")]
-pub struct YamlFormat<'de>(serde_yaml::Deserializer<'de>);
+pub struct YamlFormat {}
 
 #[cfg(feature = "yaml")]
-impl From<BufReader<File>> for YamlFormat<'_> {
-    fn from(reader: BufReader<File>) -> Self {
-        Self(serde_yaml::Deserializer::from_reader(reader))
+impl<'de, C> ConfigDeserialize<'de, C> for YamlFormat
+where
+    C: for<'de1> Deserialize<'de1> + Configuration + 'de,
+{
+    type Error = serde_yaml::Error;
+
+    fn try_config_from_reader(reader: impl std::io::Read) -> Result<C, Self::Error> {
+        serde_yaml::from_reader(reader)
     }
-}
 
-#[cfg(feature = "yaml")]
-impl<'de> From<&'de str> for YamlFormat<'de> {
-    fn from(string: &'de str) -> Self {
-        Self(serde_yaml::Deserializer::from_str(string))
-    }
-}
-
-#[cfg(feature = "json")]
-#[derive(Delegate)]
-#[delegate(Deserializer<'de1>, generics = "'de1")]
-pub struct JsonFormat<R>(serde_json::Deserializer<R>);
-
-#[cfg(feature = "json")]
-impl From<BufReader<File>> for JsonFormat<serde_json::de::IoRead<BufReader<File>>> {
-    fn from(reader: BufReader<File>) -> Self {
-        Self(serde_json::Deserializer::from_reader(reader))
+    fn try_config_from_string(string: &'de str) -> Result<C, Self::Error> {
+        serde_yaml::from_str(string)
     }
 }
 
 #[cfg(feature = "json")]
-impl<'a> From<&'a str> for JsonFormat<serde_json::de::StrRead<'a>> {
-    fn from(string: &'a str) -> Self {
-        Self(serde_json::Deserializer::from_str(string))
+pub struct JsonFormat {}
+
+#[cfg(feature = "json")]
+impl<'de, C> ConfigDeserialize<'de, C> for JsonFormat
+where
+    C: for<'de1> Deserialize<'de1> + Configuration + 'de,
+{
+    type Error = serde_json::Error;
+
+    fn try_config_from_reader(reader: impl std::io::Read) -> Result<C, Self::Error> {
+        serde_json::from_reader(reader)
     }
-}
 
-#[cfg(feature = "toml")]
-#[derive(Delegate)]
-#[delegate(Deserializer<'de1>, generics = "'de1")]
-pub struct TomlFormat<'de>(toml::Deserializer<'de>);
-
-#[cfg(feature = "toml")]
-impl<'de> From<BufReader<File>> for TomlFormat<'de> {
-    fn from(mut reader: BufReader<File>) -> Self {
-        let mut string = String::new();
-        _ = std::io::Read::read_to_string(&mut reader, &mut string);
-        Self(toml::Deserializer::new(&string))
-    }
-}
-
-#[cfg(feature = "toml")]
-impl<'de> From<&'de str> for TomlFormat<'de> {
-    fn from(string: &'de str) -> Self {
-        Self(toml::Deserializer::new(string))
+    fn try_config_from_string(string: &'de str) -> Result<C, Self::Error> {
+        serde_json::from_str(string)
     }
 }
 
@@ -636,18 +298,6 @@ use std::{
 
 #[cfg(feature = "serde")]
 use serde::de::Deserialize;
-
-#[cfg(feature = "serde")]
-use serde::de::Visitor;
-
-#[cfg(feature = "serde")]
-use ambassador::delegatable_trait_remote;
-
-#[cfg(feature = "serde")]
-use ambassador::Delegate;
-
-#[cfg(feature = "serde")]
-use serde::Deserializer;
 
 use snafu::{self, ResultExt, Snafu};
 
